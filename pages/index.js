af50e2a67e1feb61cbe27dcd1e8391faff0fe6c9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-07 20:45:36 +0100

    Cleaning Up

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
