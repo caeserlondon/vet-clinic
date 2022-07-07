3090223da540eb10ed974184c7826ebd69561c74
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-07 20:36:48 +0100

    Updating the Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
 
-const Layout = () => {
-	return <div>Layout</div>
+const Layout = ({ children }) => {
+	return (
+		<div>
+			<Navbar />
+			{children}
+			<Footer />
+		</div>
+	)
 }
 
 export default Layout
