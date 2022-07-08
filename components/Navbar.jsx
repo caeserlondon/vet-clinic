3a04fbebf7448198719a7568ae00c151ca3aa74f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-08 21:57:15 +0100

    Updating the Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<div className={styles.container}>
+			<Link href='/'>
+				<h2>
+					Vet<span>Clinic</span>
+				</h2>
+			</Link>
+			<ul className={styles.list}>
+				<li className={styles.listItem}>
+					<Link href='/'>Home</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/services'>Services</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/pet-care'>Pet Care</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/surgery'>Surgery</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/about'>About Us</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/contact-us'>Contact Us</Link>
+				</li>
+			</ul>
+		</div>
+	)
 }
 
 export default Navbar
