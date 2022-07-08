e7222c74efcc6bd5ab3d51a2dda6a109fb03fd35
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-08 21:59:41 +0100

    Creating the NotFound page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import styles from '../styles/404.module.css'
+import Link from 'next/link'
+
+export default function NotFoundPage() {
+	return (
+		<div className={styles.error}>
+			<h1>404</h1>
+			<h3>We are sorry, But the page you are looking for does not exist. </h3>
+			<Link href='/'>
+				<div className='btn'>Back To The Main Page</div>
+			</Link>
+		</div>
+	)
+}
