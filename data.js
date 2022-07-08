b601ebdcfac266c6e08ad97f69e8ca09f51dfbe3
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-08 21:55:42 +0100

    Creating the Data file

---
+++ data.js
@@ -0,0 +1,27 @@
+export const data = [
+	{
+		id: 1,
+		name: 'services',
+		title: 'Services',
+		photo: 'services.jpg',
+	},
+	{
+		id: 2,
+		name: 'pet-care',
+		title: 'Pet Care',
+		video: 'pet-care.mp4',
+		photo: 'aaa.png',
+	},
+	{
+		id: 3,
+		name: 'surgery',
+		title: 'Surgery',
+		photo: 'surgery.jpg',
+	},
+	{
+		id: 4,
+		name: 'contact-us',
+		title: 'Contact Us',
+		photo: 'contact.jpg',
+	},
+]
