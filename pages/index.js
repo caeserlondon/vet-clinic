39c6da6f5c8ebe894d9ed831ead824a1ffc94453
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-08 20:04:45 +0100

    Adding the Intro component to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
+import Intro from '../components/Intro'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		// <div className={styles.container}>
+		<div>
 			<Head>
 				<title>Vet Clinic</title>
 				<meta name='description' content='Created by CaeserLondon' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<h1>Home Page</h1>
+			<Intro />
 		</div>
 	)
 }
