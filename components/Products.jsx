f5a102eddd44d525809b954e793478bbd4d3ed2e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-09 19:45:07 +0100

    Updating the Products components

--- components/Products.jsx
+++ components/Products.jsx
@@ -1,11 +1,40 @@
-import styles from '../styles/products.module.css'
+import styles from '../styles/Products.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const Products = ({ services }) => {
+const Products = ({ products }) => {
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.title}>What We Can Do?</h1>
+			<h1 className={styles.title}></h1>
 			<h1 className={styles.subtitle}>Our Products And Services:</h1>
-			<div className={styles.products}></div>
+			<div className={styles.products}>
+				{products.map((product) => (
+					<Link href={`/products/${product.name}`} key={product.id}>
+						<div className={styles.product}>
+							<span className={styles.category}>{products.title}</span>
+							<div className={styles.media}>
+								{product.vedio ? (
+									<video
+										src={product.video}
+										autoPlay
+										loop
+										className={styles.video}
+									/>
+								) : (
+									<Image
+										src={`/images/${product.photo}`}
+										width='100%'
+										height='100%'
+										layout='responsive'
+										objectFit='cover'
+										alt={product.title}
+									/>
+								)}
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
 		</div>
 	)
 }
