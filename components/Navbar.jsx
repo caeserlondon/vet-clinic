a0aac51f3ce3eaa257fae49b15c5006b619406bf
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-11 20:04:48 +0100

    Updating the menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false)
+
 	return (
 		<div className={styles.container}>
 			<Link href='/'>
@@ -23,10 +26,37 @@ const Navbar = () => {
 					<Link href='/services/surgery'>Surgery</Link>
 				</li>
 				<li className={styles.listItem}>
-					<Link href='/services/about-us'>About Us</Link>
+					<Link href='/services/about'>About Us</Link>
 				</li>
 				<li className={styles.listItem}>
-					<Link href='/contact-us'>Contact Us</Link>
+					<Link href='/contact'>Contact Us</Link>
+				</li>
+			</ul>
+			<div className={styles.menu} onClick={() => setOpen(!open)}>
+				<div className={styles.line} />
+				<div className={styles.line} />
+				<div className={styles.line} />
+			</div>
+			<ul className={styles.menulist} style={{ right: open ? '0px' : '-50vw' }}>
+				<li className={styles.menulistItem}>
+					<div className='btn' onClick={() => setOpen(!open)}>
+						Close The Menu
+					</div>
+				</li>
+				<li className={styles.menulistItem}>
+					<Link href='/services/health-checks'>Health Checks</Link>
+				</li>
+				<li className={styles.menulistItem}>
+					<Link href='/services/pet-care'>Pet Care</Link>
+				</li>
+				<li className={styles.menulistItem}>
+					<Link href='/services/surgery'>Surgery</Link>
+				</li>
+				<li className={styles.menulistItem}>
+					<Link href='/services/about'>About Us</Link>
+				</li>
+				<li className={styles.menulistItem}>
+					<Link href='/contact'>Contact Us</Link>
 				</li>
 			</ul>
 		</div>
