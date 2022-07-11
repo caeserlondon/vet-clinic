a9e509d41d6edbf8338ee19729eb6cadba9bb93d
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-11 20:11:29 +0100

    Creating the about page

---
+++ pages/services/about.js
@@ -0,0 +1,13 @@
+import styles from '../../styles/About.module.css'
+
+const about = () => {
+	return (
+		<div className={styles.container}>
+			<div>
+				<h1>about</h1>
+			</div>
+		</div>
+	)
+}
+
+export default about
