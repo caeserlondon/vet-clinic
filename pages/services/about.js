b4e0af5030dd2598dbe6a6a48fb13ebfe640b02e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-12 20:32:09 +0100

    Creating the about page

--- pages/services/about.js
+++ pages/services/about.js
@@ -3,8 +3,23 @@ import styles from '../../styles/About.module.css'
 const about = () => {
 	return (
 		<div className={styles.container}>
-			<div>
-				<h1>about</h1>
+			<div className={styles.about}>
+				<h1> Disclaimer: </h1>
+
+				<h2>
+					This website is fictional, for personal use and non commercial purpose
+					only. Vet Clinic is<span>NOT</span>a veterinary clinic and
+					<span>NO</span>one will contact you even if you got in touch with the
+					website.
+				</h2>
+				<h3>
+					If your pet is unwell please seek professional medical help and advice
+					!!!
+				</h3>
+				<h2>
+					All media ( photos and videos ) used in the website were taken with
+					gratitude from PEXELS.COM
+				</h2>
 			</div>
 		</div>
 	)
