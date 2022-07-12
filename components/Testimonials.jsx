6d2c8b5a4d751ab49963c22a3e7f2b24e76fd234
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-12 20:28:39 +0100

    Updating Images source & adding circle class

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -6,20 +6,22 @@ import Image from 'next/image'
 const Testimonials = () => {
 	return (
 		<div className={styles.container}>
-			<Circle
-				backgroundColor='#1E90FF'
-				top='-70vh'
-				left='0'
-				right='0'
-				borderRadius='35%'
-			/>
+			<div className={styles.circle}>
+				<Circle
+					backgroundColor='#1E90FF'
+					top='-75vh'
+					left='0'
+					right='0'
+					borderRadius='40%'
+				/>
+			</div>
 			<h1 className={styles.title}>Testimonials</h1>
 
 			<div className={styles.wrapper}>
 				{users.map((user) => (
 					<div key={user.id} className={styles.card}>
 						<Image
-							src={`/images/${user.logo}`}
+							src={`${process.env.NEXT_PUBLIC_URL}/images/${user.logo}`}
 							alt='user logo'
 							width='30'
 							height='30'
@@ -27,7 +29,7 @@ const Testimonials = () => {
 						<p className={styles.comments}>{user.comment}</p>
 						<div className={styles.person}>
 							<Image
-								src={`/images/${user.avatar}`}
+								src={`${process.env.NEXT_PUBLIC_URL}/images/${user.avatar}`}
 								alt='user photo'
 								width='45'
 								height='45'
