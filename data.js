d43d00613e6ab70da28e034679c0fae6fbbbeb0d
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-12 20:20:59 +0100

    Updating the data file

--- data.js
+++ data.js
@@ -1,27 +1,152 @@
 export const data = [
 	{
 		id: 1,
-		name: 'services',
-		title: 'Services',
-		photo: 'services.jpg',
+		name: 'health-checks',
+		title: 'Health Checks',
+		video: 'health-checks/health-checks.mp4',
+		description:
+			'Regular nose-to-tail wellness exams, early detection, and age-specific assessments help keep your pet healthy.Many animals are good at hiding signs that something is wrong, so subtle changes in their health or behavior might be overlooked without preventive care.At Vet Clinic, our treatment philosophy is aimed at prevention with early diagnosis and treatment. We would love to partner with you in helping your pet live a long, happy, and healthy life.',
+		images: [
+			{
+				id: 1,
+				src: 'health-checks/health1.jpg',
+			},
+			{
+				id: 2,
+				src: 'health-checks/health2.jpg',
+			},
+			{
+				id: 3,
+				src: 'health-checks/health3.jpg',
+			},
+			{
+				id: 4,
+				src: 'health-checks/health4.jpg',
+			},
+			{
+				id: 5,
+				src: 'health-checks/health5.jpg',
+			},
+			{
+				id: 6,
+				src: 'health-checks/health6.jpg',
+			},
+			{
+				id: 7,
+				src: 'health-checks/health7.jpg',
+			},
+			{
+				id: 8,
+				src: 'health-checks/health8.jpg',
+			},
+			{
+				id: 9,
+				src: 'health-checks/health9.jpg',
+			},
+		],
 	},
 	{
 		id: 2,
 		name: 'pet-care',
 		title: 'Pet Care',
-		video: 'pet-care.mp4',
-		photo: 'aaa.png',
+		video: 'pet-care/pet-care.mp4',
+		description:
+			'From preventive care to surgery, our broad and varied service offering can meet all of your pet’s health needs in one place.We care for all animals and have developed a reputation over the years for quality, professional care with a compassionate touch.Whether you are bringing your pet in for the first time or have relied on our veterinary staff for years, we will treat your furry friend as if they were our own.',
+		images: [
+			{
+				id: 1,
+				src: 'pet-care/care1.jpg',
+			},
+			{
+				id: 2,
+				src: 'pet-care/care2.jpg',
+			},
+			{
+				id: 3,
+				src: 'pet-care/care3.jpg',
+			},
+			{
+				id: 4,
+				src: 'pet-care/care4.jpg',
+			},
+			{
+				id: 5,
+				src: 'pet-care/care5.jpg',
+			},
+			{
+				id: 6,
+				src: 'pet-care/care6.jpg',
+			},
+		],
 	},
 	{
 		id: 3,
 		name: 'surgery',
 		title: 'Surgery',
-		photo: 'surgery.jpg',
+		video: 'surgery/surgery.mp4',
+		description:
+			'From routine to advanced surgical procedures, our practice offers safe and effective options using state-of-the-art equipment and best practices.At Vet Clinic, we understand that you are probably nervous about submitting your pet to any type of surgical procedure.Our compassionate and caring professionals monitor your pet before, during, and after surgery and take exceptional care to ensure their safety and complete recovery.',
+		images: [
+			{
+				id: 1,
+				src: 'surgery/surgery1.jpg',
+			},
+			{
+				id: 2,
+				src: 'surgery/surgery2.jpg',
+			},
+			{
+				id: 3,
+				src: 'surgery/surgery3.jpg',
+			},
+			{
+				id: 4,
+				src: 'surgery/surgery4.jpg',
+			},
+			{
+				id: 5,
+				src: 'surgery/surgery5.jpg',
+			},
+			{
+				id: 6,
+				src: 'surgery/surgery6.jpg',
+			},
+		],
 	},
 	{
 		id: 4,
-		name: 'contact-us',
-		title: 'Contact Us',
-		photo: 'contact.jpg',
+		name: 'about',
+		title: 'About Us',
+		video: 'about.mp4',
+	},
+]
+
+export const users = [
+	{
+		id: 1,
+		logo: 'yelp.png',
+		avatar: 'user1.jpg',
+		comment:
+			'Truly a clinic who cares and would not hesitate to go the extra mile to do the very best for every animal. Thank you, Dr. Caeser and to your amazing team.',
+		name: 'Rachel Smith',
+		title: 'Chef',
+	},
+	{
+		id: 2,
+		logo: 'google.png',
+		avatar: 'user2.jpg',
+		comment:
+			'What an amazing staff! What a terrific veterinary clinic! They truly care about each and every animal and give such dedicated attention to every effort. Thank you so much for your help.',
+		name: 'Hanna Adams',
+		title: 'Accountant',
+	},
+	{
+		id: 3,
+		logo: 'facebook.png',
+		avatar: 'user3.jpg',
+		comment:
+			'The staffs at The VetClinic are really friendly and caring.. Dr Caeser is a very kind hearted and knowledgeable doctor. The team took very good care of Mr Whiskers. I am very thankful for them.',
+		name: 'John Doe',
+		title: 'Teacher',
 	},
 ]
